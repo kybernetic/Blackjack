9e2f3611af5e1b9171fe24325a00468ff4f332cc
kybernetic <kybernetes@tuta.io>
2018-09-28 14:33:31 +0200

    gameplay done

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,14 +2,19 @@ let playerHand = [];
 let dealerHand = [];
 const deck = new Deck();
 
-function compare(arr1, arr2) {
+function compare(playerHand, dealerHand) {
   let sum1 = 0;
   let sum2 = 0;
-  for (var index = 0; index < arr1.length; index++) {
-    sum1 += arr[index];
+  for (var index = 0; index < playerHand.length; index++) {
+    sum1 += playerHand[index].value;
   }
-  for (var index = 0; index < arr2.length; index++) {
-    sum2 += arr[index];
+  for (var index = 0; index < dealerHand.length; index++) {
+    sum2 += dealerHand[index].value;
+  }
+  if(sum1 > sum2) {
+    alert("Player wins");
+  } else {
+    alert("Player loses");
   }
   /* if sum1 and sum2 less than 21, compare them and the one which is closer
   to 21 wins */
@@ -26,18 +31,23 @@ function countForPlayer(arr) {
     alert("Player loses");
     
   }
-  console.log(sum);
+  
 }
 
 function countForDealer(arr) {
   let sum = 0;
   for (var index = 0; index < arr.length; index++) {
-    sum += arr[index];
+    sum += arr[index].value;
   }
-  if(sum > 21) {
-    alert("Player wins");
+  if(sum <= 17) {
+    alert("Dealer gets a new card");
+  } else if (sum > 17 && sum < 21) {
+    compare(playerHand, dealerHand); /* finish to write the compare func */
+  } else if (sum > 21) {
+    alert("The player wins");
+  }
 
-  }
+
 }
 
 
@@ -64,9 +74,9 @@ function initialDistribution() {
   })
 }
 
-function distribution() {
+function playerDistribution() {
   playerHand.push(deck.deal());
-  console.log(deck.deal());
+  
   let PlHand = document.getElementById('playhand');
   PlHand.innerHTML = '';
   playerHand.forEach((item, index) => {
@@ -74,13 +84,25 @@ function distribution() {
       `<div class="card face-${item.rank}-of-${item.suit}"> </div>`      
     )
   });
-  countForPlayer(playerHand);
-  
+  countForPlayer(playerHand); 
   
 }
 
+function dealerDistribution() {
+  dealerHand.push(deck.deal());
+  let DlHand = document.getElementById('dealhand');
+  DlHand.innerHTML = '';
+  dealerHand.forEach((item, index) => {
+    DlHand.innerHTML += (
+      `<div class="card face-${item.rank}-of-${item.suit}"> </div>`     
+    )
+  });
+  countForDealer(dealerHand); /* modify it for both 17 and 21 */.0
+}
 
-
+/* place the dealerDistribution function inside a bigger function which
+includes counting (both for 21 and for the first comparison between
+the two arrays) */
  
 
 
